f99026fae3880394d84318771b836517a1fb512f
anukin <anukin2611@gmail.com>
2015-09-02 17:35:32 +0530

    User can have a stream of tweets from people whom he follow

--- lib/twitter_cli/user_stream.rb
+++ lib/twitter_cli/user_stream.rb
@@ -5,7 +5,28 @@ module TwitterCli
     end
 
     def get_stream
-      ["caught a rattata", "shine on you crazy diamond", "lol"]
+      connect
+      res = @conn.exec('select tweets.tweet from tweets INNER JOIN follow ON (tweets.username = follow.following) and follow.username = $1', [@username])
+      result = aggregate_tweets(res)
+      disconnect
+      result
+    end
+
+    def connect
+      #@conn = PG.connect(:hostaddr => ENV['hostaddress'], :dbname => ENV['database'], :port => ENV['port'], :user => ENV['username'], :password => ENV['password'])
+      @conn = PG.connect(:dbname => ENV['database'])
+    end
+
+    def disconnect
+      @conn.close
+    end
+
+    def aggregate_tweets(res)
+      tweets = []
+      res.each do|row|
+          tweets << row['tweet']
+      end
+      tweets
     end
   end
 end
--- spec/twitter_cli/user_stream_spec.rb
+++ spec/twitter_cli/user_stream_spec.rb
@@ -4,7 +4,7 @@ module TwitterCli
   describe "stream" do
     let(:conn) { PG.connect(:dbname => ENV['database']) }
     let(:res_anugrah) { conn.exec('select tweets.tweet from tweets INNER JOIN follow ON (tweets.username = follow.following) and follow.username = $1', ['anugrah']) }
-    let(:res_red) { conn.exec('select tweet from tweets where username = $1', ['anugrah']) }
+    let(:res_red) { conn.exec('select tweets.tweet from tweets INNER JOIN follow ON (tweets.username = follow.following) and follow.username = $1', ['red']) }
     let(:tweets) { [] }
     def helper_get_stream(res)
       res.each do|row|
@@ -17,6 +17,12 @@ module TwitterCli
       username = 'anugrah'
       stream = Stream.new(username)
       expect(stream.get_stream).to eq(helper_get_stream(res_anugrah))
+    end
+
+    it "should return the tweets of all users which a certain user follows" do
+      username = 'red'
+      stream = Stream.new(username)
+      expect(stream.get_stream).to eq(helper_get_stream(res_red))
     end
   end
 end
