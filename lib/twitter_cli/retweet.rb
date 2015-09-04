6263dcae26e7729d628961fc6cfa699bb8ca6346
anukin <anukin2611@gmail.com>
2015-09-04 14:10:45 +0530

    Retweet can now be done only once by every user

--- lib/twitter_cli/retweet.rb
+++ lib/twitter_cli/retweet.rb
@@ -16,10 +16,37 @@ module TwitterCli
     private
 
     def post_retweet(retrieve_tweet)
-      "Successfully retweeted tweet by anugrah!"
+      @tweet = @tweeted_by + " : " + retrieve_tweet
+      validate_uniqueness(retweet_result)
+    end
+
+    def validate_uniqueness(res)
+      if res.ntuples == 1
+        "You have already retweeted this tweet"
+      else
+        prepare_insert_statement
+        tweet_res = @conn.exec_prepared('insert_tweet',[@username, @tweet ])
+        prepare_insert_retweet
+        @conn.exec_prepared('insert_retweet', [@tweet_id, @username, tweet_res[0]['id']])
+        "Successfully retweeted tweet by " + @tweeted_by + "!"
+      end
+    end
+
+    def retweet_result
+      @conn.exec('select * from retweets where retweeted_by = $1 and original_tweet_id = $2', [@username, @tweet_id])
+    end
+    
+    def prepare_insert_statement
+      @conn.prepare("insert_tweet", "insert into tweets(username, tweet) values ( $1, $2 ) returning id")
+    end
+
+    def prepare_insert_retweet
+      @conn.prepare("insert_retweet", "insert into retweets(original_tweet_id, retweeted_by, retweet_tweet_id) values ( $1, $2, $3 )")
     end
 
     def retrieve_tweet
+      @res = @conn.exec('select * from tweets where id = $1', [@tweet_id])
+      @res[0]['tweet']
     end
 
     def connect
--- spec/twitter_cli/retweet_spec.rb
+++ spec/twitter_cli/retweet_spec.rb
@@ -2,13 +2,23 @@ require 'spec_helper'
 
 module TwitterCli
   describe "retweet" do
-    it "should retweet the message according to the id, original author and tweet" do
+    it "the retweeted message must be a part of user's timeline" do
       username = 'red'
       tweeted_by = 'anugrah'
       tweet_id = 15
-      tweet = "retweeted anugrah : i like the smell of napalm in the morning"
+      tweet = "anugrah : i like the smell of napalm in the morning"
       retweet = Retweet.new(username, tweeted_by, tweet_id)
-      expect(retweet.execute).to eq("Successfully retweeted tweet by anugrah!")
+      retweet.execute
+      expect(Timeline.new('red').process).to include(tweet)
+    end
+
+    it "should allow retweet over retweet in such a way that the original author is only mentioned" do
+      username = 'red'
+      tweeted_by = 'anugrah'
+      tweet_id = 15
+      tweet = "anugrah : i like the smell of napalm in the morning"
+      retweet = Retweet.new(username, tweeted_by, tweet_id)
+      expect(retweet.execute).to eq("You have already retweeted this tweet")
     end
   end
 end
