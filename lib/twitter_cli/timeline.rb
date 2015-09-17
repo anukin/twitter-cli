58d1a2c552310b46ebb9b42b3be73f272f38dc7b
anukin <anukin2611@gmail.com>
2015-09-17 15:42:11 +0530

    timeline signature changed, connection is injected

--- lib/twitter_cli/timeline.rb
+++ lib/twitter_cli/timeline.rb
@@ -3,27 +3,23 @@ module TwitterCli
   class Timeline
     #used for creating timeline of an user
     attr_reader :user
-    def initialize(user)
+    def initialize(connection, user)
+      @conn = connection
       @user = user
     end
 
     def process
-      connect
-      @tweets = []
-      @user_result = @conn.exec('select name from users where name = $1', [@user])
-      if validate(@user_result)
-        output = "No such user exists"
+      user_result = @conn.exec('select name from users where name = $1', [@user])
+      if validate(user_result)
+        "No such user exists"
       else
-        @res = @conn.exec('select id, username, tweet from tweets where username = $1', [@user])
-        if validate(@res)
-          output = "No tweets yet"
+        res = @conn.exec('select id, username, tweet from tweets where username = $1', [@user])
+        if validate(res)
+          "No tweets yet"
         else
-          aggregate_tweets
-          output = @tweets
+          aggregate_tweets(res)
         end
       end
-      disconnect
-      output
     end
 
     def validate(result)
@@ -56,10 +52,12 @@ module TwitterCli
       @conn.close
     end
 
-    def aggregate_tweets
-      @res.each do|row|
-          @tweets << ( row['id'] + " " + row['username'] + " : " + row['tweet'] )
+    def aggregate_tweets(res)
+      tweets = []
+      res.each do|row|
+          tweets << ( row['id'] + " " + row['username'] + " : " + row['tweet'] )
       end
+      tweets
     end
   end
 end
--- spec/twitter_cli/timeline_spec.rb
+++ spec/twitter_cli/timeline_spec.rb
@@ -14,61 +14,61 @@ module TwitterCli
     end
     context "retaining tweets according to user" do
       it "should retain the dataset of those people wished upon by user" do
-        timeline = Timeline.new('anugrah')
+        timeline = Timeline.new(conn, 'anugrah')
         expect(timeline.process).to eq(helper_get_tweets(res_anugrah))
       end
 
       it "should retain the dataset of those people wished upon by user from database" do
-        timeline = Timeline.new('red')
+        timeline = Timeline.new(conn, 'red')
         expect(timeline.process).to eq(helper_get_tweets(res_red))
       end
 
       it "should give out error if user is not found" do
-        timeline = Timeline.new('reggie')
+        timeline = Timeline.new(conn, 'reggie')
         expect(timeline.process).to eq("No such user exists")
       end
 
       it "should give out no tweets if user is found but haven't tweeted yet" do
-        timeline = Timeline.new('blue')
+        timeline = Timeline.new(conn, 'blue')
         expect(timeline.process).to eq("No tweets yet")
       end
     end
 
     context "equality" do
       it "should be reflexive in nature" do
-        timeline_1 = Timeline.new('red')
-        expect(timeline_1).to eq(Timeline.new('red'))
+        timeline_1 = Timeline.new(conn, 'red')
+        expect(timeline_1).to eq(Timeline.new(conn, 'red'))
       end
 
       it "should be symmetric in nature" do
-        timeline_1 = Timeline.new('red')
-        timeline_2 = Timeline.new('red')
+        timeline_1 = Timeline.new(conn, 'red')
+        timeline_2 = Timeline.new(conn, 'red')
         expect(timeline_1).to eq(timeline_2)
         expect(timeline_2).to eq(timeline_1)
       end
 
       it "should be transitive in nature" do
-        timeline_1 = Timeline.new('red')
-        timeline_2 = Timeline.new('red')
-        timeline_3 = Timeline.new('red')
+        timeline_1 = Timeline.new(conn, 'red')
+        timeline_2 = Timeline.new(conn, 'red')
+        timeline_3 = Timeline.new(conn, 'red')
         expect(timeline_1).to eq(timeline_2)
         expect(timeline_2).to eq(timeline_3)
         expect(timeline_1).to eq(timeline_3)
       end
 
       it "is not equal to nil" do
-        timeline_1 = Timeline.new('red')
+        timeline_1 = Timeline.new(conn, 'red')
         expect(timeline_1).to_not eq(nil)
       end
 
       it "is not equal for different types" do
-        timeline_1 = Timeline.new('red')
+        timeline_1 = Timeline.new(conn, 'red')
         expect(timeline_1).to_not eq(Object.new)
       end
 
       it "hashes must be same if objects are same" do
-        timeline_1 = Timeline.new('red')
-        timeline_2 = Timeline.new('red')
+        timeline_1 = Timeline.new(conn, 'red')
+        timeline_2 = Timeline.new(conn, 'red')
         expect(timeline_1.hash).to eq(timeline_2.hash)
       end
     end
