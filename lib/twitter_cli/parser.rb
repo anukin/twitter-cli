496c5f3a9ad3a85a5b94a020c2aa63f2854addb0
anukin <anukin2611@gmail.com>
2015-09-21 16:11:52 +0530

    Refactored parser connection is injected

--- lib/twitter_cli/parser.rb
+++ lib/twitter_cli/parser.rb
@@ -1,7 +1,8 @@
 module TwitterCli
   class Parser
     #parser parses the input using some logic
-    def initialize(input, name = 0, password = 0)
+    def initialize(connection, input, name = 0, password = 0)
+      @conn = connection
       @input = input
       @name = name
       @password = password
@@ -24,26 +25,16 @@ module TwitterCli
     private
 
     def get_timeline
-      connect
       result = Timeline.new(@conn, @name)
-      disconnect
       result
     end
 
     def get_registered
-      UserRegistration.new(@name, @password)
+      UserRegistration.new(@conn, @name, @password)
     end
 
     def login
-      UserLogin.new(@name, @password)
-    end
-
-    def connect
-      @conn = PG.connect(:hostaddr => ENV['hostaddress'], :dbname => ENV['database'], :port => ENV['port'], :user => ENV['username'], :password => ENV['password'])
-    end
-
-    def disconnect
-      @conn.close
+      UserLogin.new(@conn, @name, @password)
     end
   end
 end
--- spec/twitter_cli/parser_spec.rb
+++ spec/twitter_cli/parser_spec.rb
@@ -5,18 +5,18 @@ module TwitterCli
     context "parsing" do
       let(:conn) { PG.connect(:dbname => ENV['database']) }
       it "should parse according to the inputs" do
-        parser = Parser.new('timeline', 'red')
+        parser = Parser.new(conn, 'timeline', 'red')
         expect(parser.parse).to eq(Timeline.new(conn, 'red'))
       end
 
       it "should parse according to the inputs" do
-        parser = Parser.new('register', 'lol', 'lol')
-        expect(parser.parse).to eq(UserRegistration.new('lol', 'lol'))
+        parser = Parser.new(conn, 'register', 'lol', 'lol')
+        expect(parser.parse).to eq(UserRegistration.new(conn, 'lol', 'lol'))
       end
 
       it "should parse according to the inputs" do
-        parser = Parser.new('login', 'bulla', 'gunda')
-        expect(parser.parse).to eq(UserLogin.new('bulla', 'gunda'))
+        parser = Parser.new(conn, 'login', 'bulla', 'gunda')
+        expect(parser.parse).to eq(UserLogin.new(conn, 'bulla', 'gunda'))
       end
     end
   end
