6050c16dd3d8b04a9c7b1c66298b7c2be4af9973
anukin <anukin2611@gmail.com>
2015-09-21 16:10:01 +0530

    Refactored user login connection is injected

--- lib/twitter_cli/user_login.rb
+++ lib/twitter_cli/user_login.rb
@@ -2,16 +2,23 @@ module TwitterCli
   class UserLogin
     attr_reader :username, :password
     
-    def initialize(username, password)
+    def initialize(connection, username, password)
+      @conn = connection
       @username = username
       @password = password
     end
 
     def process
-      connect
-      output = login
-      disconnect
-      output
+      res = @conn.exec("select name, password from users where name = $1", [@username])
+      if res.ntuples == 0
+        "Access denied! No user by that name."
+      else
+        if password_validation(res)
+          Stream.new(@conn, @username).get_stream
+        else
+          "Access denied! Check your password."
+        end
+      end
     end
 
     def ==(other)
@@ -31,18 +38,6 @@ module TwitterCli
     end
 
     private
-    def login
-      res = @conn.exec("select name, password from users where name = $1", [@username])
-      if res.ntuples == 0
-        "Access denied! No user by that name."
-      else
-        if password_validation(res)
-          Stream.new(@conn, @username).get_stream
-        else
-          "Access denied! Check your password."
-        end
-      end
-    end
 
     def password_validation(res)
       res[0]['password'] == @password
--- spec/twitter_cli/user_login_spec.rb
+++ spec/twitter_cli/user_login_spec.rb
@@ -5,57 +5,57 @@ module TwitterCli
     let(:conn) { PG.connect(:dbname => ENV['database']) }
     context Process do
       it "should let the user sign in according to the name and password and return the user stream" do
-        user_login = UserLogin.new("anugrah", "megamind")
+        user_login = UserLogin.new(conn, "anugrah", "megamind")
         stream = Stream.new(conn, 'anugrah')
         expect(user_login.process).to eq(stream.get_stream)
       end
 
       it "should let the user login using valid credentials" do
-        user_login = UserLogin.new("bulla", "gunda")
+        user_login = UserLogin.new(conn, "bulla", "gunda")
         expect(user_login.process).to eq("Access denied! No user by that name.")
       end
 
       it "should not let the user login using arbitrary values" do
-        user_login = UserLogin.new("red", "gunda")
+        user_login = UserLogin.new(conn, "red", "gunda")
         expect(user_login.process).to eq("Access denied! Check your password.")
       end
     end
 
     context "equality" do
       it "should be reflexive in nature" do
-        user_login_1 = UserLogin.new('red','red')
-        expect(user_login_1).to eq(UserLogin.new('red', 'red'))
+        user_login_1 = UserLogin.new(conn, 'red','red')
+        expect(user_login_1).to eq(UserLogin.new(conn, 'red', 'red'))
       end
 
       it "should be symmetric in nature" do
-        user_login_1 = UserLogin.new('red', 'red')
-        user_login_2 = UserLogin.new('red', 'red')
+        user_login_1 = UserLogin.new(conn, 'red', 'red')
+        user_login_2 = UserLogin.new(conn, 'red', 'red')
         expect(user_login_1).to eq(user_login_2)
         expect(user_login_2).to eq(user_login_1)
       end
 
       it "should be transitive in nature" do
-        user_login_1 = UserLogin.new('red', 'red')
-        user_login_2 = UserLogin.new('red', 'red')
-        user_login_3 = UserLogin.new('red', 'red')
+        user_login_1 = UserLogin.new(conn, 'red', 'red')
+        user_login_2 = UserLogin.new(conn, 'red', 'red')
+        user_login_3 = UserLogin.new(conn, 'red', 'red')
         expect(user_login_1).to eq(user_login_2)
         expect(user_login_2).to eq(user_login_3)
         expect(user_login_1).to eq(user_login_3)
       end
 
       it "is not equal to nil" do
-        user_login_1 = UserLogin.new('red', 'red')
+        user_login_1 = UserLogin.new(conn, 'red', 'red')
         expect(user_login_1).to_not eq(nil)
       end
 
       it "is not equal for different types" do
-        user_login_1 = UserLogin.new('red', 'red')
+        user_login_1 = UserLogin.new(conn, 'red', 'red')
         expect(user_login_1).to_not eq(Object.new)
       end
 
       it "hashes must be same if objects are same" do
-        user_login_1 = UserLogin.new('red', 'red')
-        user_login_2 = UserLogin.new('red', 'red')
+        user_login_1 = UserLogin.new(conn, 'red', 'red')
+        user_login_2 = UserLogin.new(conn, 'red', 'red')
         expect(user_login_1.hash).to eq(user_login_2.hash)
       end
     end
